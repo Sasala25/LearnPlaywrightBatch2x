82ec6aef76e9eeda9cafd2a70b555747ad5a9715
Sasala25 <sasala25@users.noreply.github.com>
2026-06-09 00:29:02 +0530

    fix: add missing greetByName and return value in begger function

---
+++ chapter_12_Functions/Type-2_function.js
@@ -0,0 +1,14 @@
+let result = greetByName("Soundarya");
+console.log(result); // Hello Soundarya
+
+function begger(money) {
+    return "Give me " + money;
+
+}
+
+let returnMesomething = begger(100);
+console.log(returnMesomething); // Give me 100
+
+function greetByName(name) {
+    return "Hello " + name;
+}
